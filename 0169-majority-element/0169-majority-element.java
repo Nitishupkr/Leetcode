a4815776c48ef1ba882ddc3366dd8f544feaa6f5
Nitishupkr <95420063+Nitishupkr@users.noreply.github.com>
2022-11-17 01:56:34 +0530

    Time: 2 ms (93.13%), Space: 55.8 MB (55.90%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+      int count=1;
+        int max=nums[0];
+            for(int i=1;i<nums.length;i++){
+                if(count==0){
+                    count++;
+                    max=nums[i];
+                }else if(max==nums[i]){
+                    count++;
+                }else
+                    count--;
+            }
+        return max;
     }
 }
