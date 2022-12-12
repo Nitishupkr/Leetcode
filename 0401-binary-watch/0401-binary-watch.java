d5db48d5325027fd9f56b0f32f7447f49500bf12
Nitishupkr <95420063+Nitishupkr@users.noreply.github.com>
2022-12-13 00:51:48 +0530

    Time: 13 ms (65.79%), Space: 43.3 MB (28.34%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.java
+++ 0401-binary-watch/0401-binary-watch.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<String> readBinaryWatch(int turnedOn) {
-        List<String>list=new ArrayList<>();
-        for(int i =0;i<12;i++){
-            for(int j=0;j<60;j++){
-                if(Integer.bitCount(i)+Integer.bitCount(j)==turnedOn){
-                list.add(String.format("%d:%02d",i,j));    
+            ArrayList<String> result = new ArrayList();
+        for(int hour=0; hour<12; hour++){
+            for(int minute=0; minute<60; minute++){
+                if(Integer.bitCount(hour) + Integer.bitCount(minute) == turnedOn){
+                    if(minute<10) result.add(String.format("%d:0%d",hour,minute));
+                    else result.add(String.format("%d:%d",hour,minute));
                 }
-}
+            }
         }
-            return list;
-        }
+        return result;
     }
- 
+}
